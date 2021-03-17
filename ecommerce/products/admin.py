51515b47458fe82fd0aeacff60b91a76fe73bdc1
psuman10 <psuman9840@gmail.com>
2021-03-17 11:52:33 +0545

    first commit

---
+++ ecommerce/products/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import Product,Cart,OrderPlaced,Customer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','title','selling_price','discounted_price','description','brand','category','product_image']
+admin.site.register(Product,ProductAdmin)
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display=['id','user','product','quantity']
+admin.site.register(Cart,CartAdmin)
+
+
+
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','customer_info','product_info','product','quantity','ordered_date','status']
+admin.site.register(OrderPlaced,OrderPlacedAdmin)
+
+
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','locality','city','state']
+admin.site.register(Customer,CustomerModelAdmin)
+
+
+
+
