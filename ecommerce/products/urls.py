6f5b8ef125a97fe02c4d8d211bb638b8f499e6ca
psuman10 <psuman9840@gmail.com>
2021-03-18 16:51:58 +0545

    updated urls

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,11 +1,15 @@
-
 from django.urls import path
-from .import views
-
-
-
+from app import views
 urlpatterns = [
-    path('',views.index),
-  
-
+    path('', views.home),
+    path('product-detail/', views.product_detail, name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    path('changepassword/', views.change_password, name='changepassword'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.customerregistration, name='customerregistration'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
