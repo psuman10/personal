39e06840a4614a046a018263dc23da9844d61aeb
psuman10 <psuman9840@gmail.com>
2021-03-19 11:14:35 +0545

    changed image settings

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home),
     path('product-detail/', views.product_detail, name='product-detail'),
@@ -12,4 +16,4 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
