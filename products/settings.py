e38ceb73ea61f9115fb25f420778d5e470612389
psuman10 <psuman9840@gmail.com>
2021-03-21 09:59:38 +0545

    half work done with carousel

--- products/settings.py
+++ products/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'products.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
